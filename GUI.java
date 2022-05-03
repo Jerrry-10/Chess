d0e02edb3b477ae9cd40f086eb0bf3c70d7c5b99
kreid86 <93731916+kreid86@users.noreply.github.com>
2022-05-02 23:33:29 -0400

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -73,7 +73,7 @@ public class GUI {
 	public static void display()  {
 		BufferedImage all= null;
 		try{
-			all = ImageIO.read(new File("C:\\Users\\Kevin\\Desktop\\MyWorkspace\\CSC 330 N\\src\\chess\\chess.png"));
+			all = ImageIO.read(new File(""C:\\Users\\Jerry\\eclipse-workspace\\CSC330\\src\\chess\\chess.png""));
 		} catch(IOException e) {}
         Image imgs[]=new Image[12];
         int ind=0;
